de68b4897b4f92e29080023121eb4304254cd069
leonardo-policarpo <leo.dumont@outlook.com>
2023-10-03 12:26:40 -0300

    loading

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -7,12 +7,18 @@ import { Container } from '../../styles/GlobalStyles';
 import { AlunoContainer, ProfilePicture } from './styles';
 import axios from '../../services/axios';
 
+import Loading from '../../components/Loading';
+
 export default function Alunos() {
   const [alunos, setAlunos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     async function getData() {
+      setIsLoading(true);
       const response = await axios.get('/alunos');
       setAlunos(response.data);
+      setIsLoading(false);
     }
 
     getData();
@@ -20,6 +26,8 @@ export default function Alunos() {
 
   return (
     <Container>
+      <Loading isLoading={isLoading} />
+
       <h1>Alunos</h1>
 
       <AlunoContainer>
